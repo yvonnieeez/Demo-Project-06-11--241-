3f7c2a64ba8a0b65bf3b891101e407ce6f79da41
Montaha Zaman <montahazaman2006@gmail.com>
2026-07-21 14:07:40 +0600

    Added GPA tests

--- test.sh
+++ test.sh
@@ -6,3 +6,7 @@ gcc -I modules tests/test_course_result.c modules/course.c modules/courseResult.
 test_course_result.out
 chmod +x test_course_result.out
 ./test_course_result.out
+gcc -I modules tests/test_gpa.c modules/course.c modules/courseResult.c modules/gpa.c -o
+test_gpa.out
+chmod +x test_gpa.out
+./test_gpa.out
