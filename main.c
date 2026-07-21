ee847a144f138f28b001c6f1dc0f4712c509daf6
Montaha Zaman <montahazaman2006@gmail.com>
2026-07-21 22:26:56 +0600

    Displayed student ranking in main

--- main.c
+++ main.c
@@ -1,20 +1,32 @@
-#ifndef STUDENT_H
-#define STUDENT_H
-
+#include <stdio.h>
+#include "course.h"
 #include "courseResult.h"
+#include "student.h"
 
-typedef struct Student
+int main()
 {
-    char id[20];
-    char name[80];
-    CourseResult courseResults[100];
-    int n_results;
-    double cgpa;
-} Student;
+    Course cse4107 = createCourse("CSE 4107", "Structured Programming I", 3.0, 1);
+    Course cse4108 = createCourse("CSE 4108", "Structured Programming I Lab", 1.5, 1);
 
-Student createStudent(char id[], char name[]);
-void addCourseResultToStudent(Student *student, CourseResult result);
-void viewStudent(Student student);
-void sortStudentsByCGPA(Student students[], int n_students);
+    Student students[3] = {
+        createStudent("240041001", "Alice"),
+        createStudent("240041002", "Bob"),
+        createStudent("240041003", "Carol")
+    };
 
-#endif
+    addCourseResultToStudent(&students[0], createCompletedCourseResult(&cse4107, 252));
+    addCourseResultToStudent(&students[0], createCompletedCourseResult(&cse4108, 135));
+    addCourseResultToStudent(&students[1], createCompletedCourseResult(&cse4107, 228));
+    addCourseResultToStudent(&students[1], createCompletedCourseResult(&cse4108, 123));
+    addCourseResultToStudent(&students[2], createCompletedCourseResult(&cse4107, 273));
+    addCourseResultToStudent(&students[2], createCompletedCourseResult(&cse4108, 130.5));
+
+    sortStudentsByCGPA(students, 3);
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d. ", i + 1);
+        viewStudent(students[i]);
+    }
+
+    return 0;
+}
