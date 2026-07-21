5f622fb7c778091cadb4d68cff6a7460321e253b
Montaha Zaman <montahazaman2006@gmail.com>
2026-07-21 13:57:24 +0600

    Connected course results to main

--- main.c
+++ main.c
@@ -1,20 +1,28 @@
+#include <stdio.h>
 #include "course.h"
+#include "courseResult.h"
 
 int main()
 {
     Course courses[1000];
+    CourseResult results[1000];
     int n_courses = 0;
 
     courses[n_courses++] = createCourse("CSE 4107", "Structured Programming I", 3.0);
     courses[n_courses++] = createCourse("CSE 4108", "Structured Programming I Lab", 1.5);
     courses[n_courses++] = createCourse("CSE 4203", "Discrete Mathematics", 3.0);
-    courses[n_courses++] = createCourse("CSE 4205", "Digital Logic Design", 3.0);
-    courses[n_courses++] = createCourse("Hum 4244", "English II", 2.0);
-    courses[n_courses++] = createCourse("Phy 4242", "Physics II Lab", 1.5);
 
     for (int i = 0; i < n_courses; i++)
     {
-        viewCourse(courses[i]);
+        double marks;
+        printf("Marks for %s: ", courses[i].name);
+        scanf("%lf", &marks);
+        results[i] = createCourseResult(&courses[i], marks);
+    }
+
+    for (int i = 0; i < n_courses; i++)
+    {
+        viewCourseResult(results[i]);
     }
 
     return 0;
